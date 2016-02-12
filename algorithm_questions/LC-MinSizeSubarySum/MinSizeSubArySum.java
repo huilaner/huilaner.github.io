376ac5508a69ab29cefb9b22a8b41a6ab48c0f34
huilaner <huibiology@gmail.com>
2016-02-12 15:00:54 -0500

    Nlog(N) method for min size subary sum

--- algorithm_questions/LC-MinSizeSubarySum/MinSizeSubArySum.java
+++ algorithm_questions/LC-MinSizeSubarySum/MinSizeSubArySum.java
@@ -20,3 +20,29 @@ public class Solution {
         return (min == Integer.MAX_VALUE) ? 0 : min;
     }
 }
+
+//O(nLogN) method
+//the cumulative sum must be strictly increasing. Then, a subarray sum can expressed as the difference between two cumulative sum. Hence, given a start index for the cumulative sum array, the other end index can be searched using binary search.
+public class Solution {
+    public int minSubArrayLen(int s, int[] nums) {
+        if(nums.length == 0) return 0;
+        int[] sum = new int[nums.length+1];
+        int min = Integer.MAX_VALUE;
+        for(int i = 1; i < sum.length; i++) sum[i] = sum[i-1] + nums[i-1];
+        for(int i = 0; i < sum.length; i++){
+            int end = binarySearch(i, sum.length-1, s+sum[i], sum);
+            if(end >= sum.length) break;//out of bound
+            min = Math.min(min, end-i);
+        }
+        return (min == Integer.MAX_VALUE) ? 0 : min;
+    }
+
+    private int binarySearch(int L, int R, int t, int[] ary){
+        while(L<=R){
+            int M = (L+R)/2;
+            if(ary[M] >= t) R = M-1;
+            else L = M+1;
+        }
+        return L;
+    }
+}
